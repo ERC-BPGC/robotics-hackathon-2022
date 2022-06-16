69bb96751d0192c4ac428aa9e3ef23c3cb9a7a2e
AdityaParandekar <84431866+AdityaParandekar@users.noreply.github.com>
2022-06-16 19:00:02 +0530

    Delete Automation directory

--- Automation/maze_solver/src/scripts/colour_detection.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env python3
-#You need to name this node "colour_detector"
--- Automation/maze_solver/src/scripts/controller.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-# Remember to change the coordinates recieved by the planner from (X, Y) to (X + 1.843515, Y + 0.6581).
-#you need to name this node "omnibase_controller"
